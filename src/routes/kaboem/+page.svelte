94d104df80566e726581df4bb43cefe5190bed4d
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-01-21 17:44:24 +0100

    start countdown when playing

--- src/routes/kaboem/+page.svelte
+++ src/routes/kaboem/+page.svelte
@@ -2,13 +2,15 @@
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	onMount(() => {
-		setTimeout(() => {
-			goto('/flag');
-		}, 2000);
+		document.getElementById('boem').addEventListener('play', () => {
+			setTimeout(() => {
+				goto('/flag');
+			}, 2000);
+		});
 	});
 </script>
 
-<video autoplay class="h-screen w-screen object-cover">
+<video autoplay class="h-screen w-screen object-cover" id="boem">
 	<source src="/onee.mp4" type="video/mp4" />
 	Your browser does not support the video tag.
 </video>
