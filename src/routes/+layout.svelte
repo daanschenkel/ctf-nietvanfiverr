6a6f7be3c090c6dc8a9b9821b45e2910c06b0457
Daan <20974756+daanschenkel@users.noreply.github.com>
2024-01-21 18:39:19 +0100

    improve notice

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,19 @@
 	import '../app.pcss';
 	onMount(() => {
 		document.cookie = 'hint=kijk naar localStorage; path=/';
+		console.log(
+			`%cSTOP!`,
+			'color: red; font-size: 3rem; font-weight: bold; text-shadow: 0 0 10px red;'
+		);
+		console.log(
+			`%cHet is tijdens deze CTF niet de bedoeling de source code te bekijken of aan te passen!`,
+			'color: #e5e5e5; font-size: 1.5rem; font-weight: bold;'
+		);
+		console.log(
+			`%cEr is wel iets in devtools wat je kan helpen.`,
+			'color: #e5e5e5; font-size: 1.5rem; font-weight: bold;'
+		);
+		console.log('%chttps://daanschenkel.nl', 'color: #e5e5e5; font-size: 1rem; font-weight: bold;');
 	});
 </script>
 
